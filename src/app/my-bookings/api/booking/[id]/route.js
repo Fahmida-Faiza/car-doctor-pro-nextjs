ee142ecff66fb760b68c29069e1028201ec47285
Fahmida Faiza <fahmidafaiza57@gmail.com>
2024-09-18 19:29:27 +0600

    updated api

--- src/app/my-bookings/api/booking/[id]/route.js
+++ src/app/my-bookings/api/booking/[id]/route.js
@@ -19,23 +19,40 @@ export const DELETE = async (request, { params }) => {
 };
 
 
-// update
-// export const PATCH = async (request, { params }) => {
-//   const db = await connectDB();
-//   const bookingsCollection = db.collection("bookings");
-//   try {
-//     const resp = await bookingsCollection.deleteOne({
-//       _id: new ObjectId(params.id),
-//     });
-//     return NextResponse.json({
-//       message: "deleted the booking",
-//       response: resp,
-//     });
-//   } catch (error) {
-//     return NextResponse.json({ message: "Something Went Wrong" });
-//   }
-// };
+////////////////update
+export const PATCH = async (request, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = db.collection("bookings");
+  const {date, phone, address} = await request.json()
+  try {
+    const resp = await bookingsCollection.
+    updateOne(
+      {
+      _id: new ObjectId(params.id) },
+      {
+          $set:{
 
+           date,
+           phone,
+           address,
+          },
+        },
+          {
+            upsert: true
+          }
+      
+    
+    
+  );
+    return NextResponse.json({
+      message: "updated the booking",
+      response: resp,
+    });
+  } catch (error) {
+    return NextResponse.json({ message: "Something Went Wrong" });
+  }
+};
+// ///////////////
 
 
 
